0b8bb87365c2bf35555971437e378d59c54756c9
Ning Guo <guoning.gn@gmail.com>
2017-07-02 01:07:23 +0800

    feature: 解耦 rpc service url配置

--- simple-consumer-movie/src/main/java/top/ningg/cloud/controller/MovieController.java
+++ simple-consumer-movie/src/main/java/top/ningg/cloud/controller/MovieController.java
@@ -1,5 +1,6 @@
 package top.ningg.cloud.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,11 @@ public class MovieController {
     @Resource
     private RestTemplate restTemplate;
 
+    @Value("${user.serviceUrl}")
+    private String userServiceUrl;
+
     @GetMapping("/user/{userId}")
     public User getUserById(@PathVariable("userId") long userId) {
-        return restTemplate.getForObject("http://localhost:8000/" + userId, User.class);
+        return restTemplate.getForObject(userServiceUrl + userId, User.class);
     }
 }
