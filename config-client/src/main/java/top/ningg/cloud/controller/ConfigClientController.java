f4c1c32d8ecc027cb00efc96f35140b03e3f4dc1
Ning Guo <guoning.gn@gmail.com>
2017-07-02 18:20:10 +0800

    feature: config server and client combine with eureka server

--- config-client/src/main/java/top/ningg/cloud/controller/ConfigClientController.java
+++ config-client/src/main/java/top/ningg/cloud/controller/ConfigClientController.java
@@ -1,12 +1,14 @@
 package top.ningg.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope
+@EnableDiscoveryClient
 public class ConfigClientController {
 
     @Value("${profile}")
