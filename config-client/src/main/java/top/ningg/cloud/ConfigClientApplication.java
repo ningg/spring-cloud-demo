bfd8748031fb117333ca055efa04ae9caa325194
Ning Guo <guoning.gn@gmail.com>
2017-07-02 18:06:29 +0800

    feature: config client

---
+++ config-client/src/main/java/top/ningg/cloud/ConfigClientApplication.java
@@ -0,0 +1,12 @@
+package top.ningg.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+}
