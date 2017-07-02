bc151c3076fc9fca62fec6e339f8dd3d5eba682b
Ning Guo <guoning.gn@gmail.com>
2017-07-02 16:04:09 +0800

    feature: 依赖 eureka 进行服务注册和服务发现

--- simple-consumer-movie/src/main/java/top/ningg/cloud/SimpleConsumerMovieApplication.java
+++ simple-consumer-movie/src/main/java/top/ningg/cloud/SimpleConsumerMovieApplication.java
@@ -2,10 +2,12 @@ package top.ningg.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SimpleConsumerMovieApplication {
 
     @Bean
--- simple-consumer-movie/src/main/java/top/ningg/cloud/controller/MovieController.java
+++ simple-consumer-movie/src/main/java/top/ningg/cloud/controller/MovieController.java
@@ -1,6 +1,8 @@
 package top.ningg.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 import top.ningg.cloud.model.User;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class MovieController {
@@ -18,8 +21,19 @@ public class MovieController {
     @Value("${user.serviceUrl}")
     private String userServiceUrl;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @Value("${user.serviceName}")
+    private String userServiceName;
+
     @GetMapping("/user/{userId}")
     public User getUserById(@PathVariable("userId") long userId) {
         return restTemplate.getForObject(userServiceUrl + userId, User.class);
+    }
+
+    @GetMapping("/userInstance")
+    public List<ServiceInstance> getUserServiceList() {
+        return discoveryClient.getInstances(userServiceName);
     }
 }
